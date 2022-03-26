c3fbd346fbd8c5efdc1f00eedeeb9d255c8eb3e5
Juan F. Bermejo <jfbermejo@begafa.com>
2022-03-26 12:28:23 +0100

    Ingredientes en las recetas

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,29 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe('Another Test Recipe', 'This is another simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+        new Recipe(
+            'Tortilla de patata',
+            'Estupenda y típica tortilla de patatas',
+            'https://static1.abc.es/media/bienestar/2020/05/22/tortilla-patatas-k9tF--1200x630@abc.jpeg',
+            [
+                new Ingredient('Patata', 4),
+                new Ingredient('Cebolla', 1),
+                new Ingredient('Huevo', 6),
+                new Ingredient('Aceite de oliva', 1),
+            ]
+            ),
+
+        new Recipe(
+            'Verduras al horno',
+            'Selección de verduras al horno saludables',
+            'https://www.recetasderechupete.com/wp-content/uploads/2019/12/Verduras-al-horno.jpg',
+            [
+                new Ingredient('Berenjena', 1),
+                new Ingredient('Calabacín', 2),
+                new Ingredient('Pimiento verde', 1),
+                new Ingredient('Pimiento rojo', 1),
+            ]
+            )
     ];
 
     getRecipes() {
